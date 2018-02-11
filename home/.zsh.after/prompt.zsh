6c5c8798233f9aa9ec651b1711ea46dcd869ca2e
Niklas Herder <niklas.herder@gmail.com>
2018-02-11 09:56:38 +0100

    Add input mode to PS1, add jk for <ESC>
    Split Brewfiles by os,ssid,domain

--- home/.zsh.after/prompt.zsh
+++ home/.zsh.after/prompt.zsh
@@ -1,11 +1,12 @@
 # From https://www.reddit.com/r/vim/comments/7wj81e/you_can_get_vim_bindings_in_zsh_and_bash/du0ui58/
+autoload -U colors && colors
 PS1+='${VIMODE}'
 #   '$' for normal insert mode
 #   a big red 'I' for command mode - to me this is 'NOT insert' because red
 function zle-line-init zle-keymap-select {
-    DOLLAR='%B%F{green}$%f%b '
-    GIANT_I='%B%F{red}I%f%b '
-    VIMODE="${${KEYMAP/vicmd/$GIANT_I}/(main|viins)/$DOLLAR}"
+    INSERT='%F{yellow}i%f%b '
+    NORMAL='%F{blue}n%f%b '
+    VIMODE="${${KEYMAP/vicmd/$NORMAL}/(main|viins)/$INSERT}"
     zle reset-prompt
 }
 zle -N zle-line-init
