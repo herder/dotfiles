1a9bd19fda8db14d22148e7d40d8aa779420d64d
Niklas Herder <niklas.herder@gmail.com>
2018-06-26 09:49:21 +0200

    Fix Ctrl-T rg output so that it silences errors

--- home/.zsh.after/fzf.zsh
+++ home/.zsh.after/fzf.zsh
@@ -12,4 +12,5 @@ fi
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND="rg --files --no-ignore --hidden --follow --glob '!{.git,Caches,node_modules}/*' 2>/dev/null"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
