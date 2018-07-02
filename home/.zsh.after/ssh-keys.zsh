1b14af1fb073a3343e91c1c02665b82bad626b59
Niklas Herder <niklas.herder@gmail.com>
2018-07-02 22:01:10 +0200

    Extract ssh key search to function
    To try to make it faster in the future

--- home/.zsh.after/ssh-keys.zsh
+++ home/.zsh.after/ssh-keys.zsh
@@ -1,2 +1,10 @@
 #Add all ssh keys under the keys dir to ssh agent
-for i in `gfind ~/.ssh/keys -type f | egrep -v '.pub$'` ; do ssh-add -K $i ; done
+
+function add_key {
+#  echo "Add file $1"
+  ssh-add -K "$1"
+}
+
+for i in `gfind ~/.ssh/keys -type f | egrep -v '.pub$'` ; do add_key "$i" ; done
+
+
