2617407e8cac65e37f21910f8126b878a606c6cc
Niklas Herder <niklas.herder@gmail.com>
2018-07-29 07:40:25 +0200

    Only load key if not already there

--- home/.zsh.after/ssh-keys.zsh
+++ home/.zsh.after/ssh-keys.zsh
@@ -1,8 +1,9 @@
 #Add all ssh keys under the keys dir to ssh agent
 
 function add_key {
+   key=$1
 #  echo "Add file $1"
-  ssh-add -K "$1"
+   ssh-add -l |grep -q `ssh-keygen -lf $key  | awk '{print $2}'` || ssh-add -K "$key"
 }
 
 for i in `gfind ~/.ssh/keys -type f | egrep -v '.pub$'` ; do add_key "$i" ; done
