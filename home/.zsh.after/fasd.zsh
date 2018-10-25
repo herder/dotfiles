a2b7ff2a27feb781d0c08502d7fab07c94b8f5fa
Niklas Herder <niklas.herder@gmail.com>
2018-10-25 07:32:41 +0200

    Add fasd+fzf aliases

---
+++ home/.zsh.after/fasd.zsh
@@ -0,0 +1,51 @@
+#
+# only init if installed.
+fasd_cache="$HOME/.fasd-init-bash"
+if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+  eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)" >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
+
+
+# ( copied from https://raw.githubusercontent.com/anandpiyer/.dotfiles/master/.zshrc )
+# fasd & fzf - jump using `fasd` if argument is given, filter output of `fasd`
+# using `fzf` otherwise.
+unalias j 2>/dev/null
+j() {
+    [ $# -gt 0 ] && fasd_cd -d "$*" && return
+    local dir
+    dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+}
+
+# fasd & fzf - use $EDITOR to edit file. Pick best matched file using `fasd`
+# if argument given, else use `fzf` to filter `fasd` output.
+unalias e 2>/dev/null
+e() {
+    [ $# -gt 0 ] && fasd -f -e ${EDITOR} "$*" && return
+    local file
+    file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort +m)" && ${EDITOR} "${file}" || return 1
+}
+
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fe() {
+  local files
+  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+}
+
+# fasd & fzf - open finder. If argument given, use `fasd` to pick the best match
+# else use `fzf` to select from `fasd` results.
+unalias o 2>/dev/null
+o() {
+    [ $# -gt 0 ] && fasd -a -e open "$*" && return
+    local res
+    res="$(fasd -Rla "$1" | fzf -1 -0 --no-sort +m)"
+    if [[ -d "${res}" ]]; then
+       open "${res}"
+    else
+       open "$(dirname "$res")"
+    fi
+}
