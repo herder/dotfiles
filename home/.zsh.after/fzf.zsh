a1dcab38e5c9d5a9c284d6dc5d8994316a1b5de6
Niklas Herder <niklas.herder@gmail.com>
2019-06-25 11:07:40 -0400

    Don't use hardcoded Brew path

--- home/.zsh.after/fzf.zsh
+++ home/.zsh.after/fzf.zsh
@@ -1,16 +1,17 @@
 # Setup fzf
 # ---------
+BREW_HOME=$(brew --prefix)
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+  export PATH="$PATH:$BREW_HOME/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$BREW_HOME/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$BREW_HOME/opt/fzf/shell/key-bindings.zsh"
 
 export FZF_DEFAULT_COMMAND="rg --files --no-ignore --hidden --follow --glob '!{.git,Caches,node_modules}/*' 2>/dev/null"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
