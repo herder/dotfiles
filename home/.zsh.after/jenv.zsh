f3c31880c97070091b67001047d80ddc64bf979e
Niklas Herder <niklas.herder@gmail.com>
2020-01-21 09:46:06 +0100

    Activate jenv export plugin

--- home/.zsh.after/jenv.zsh
+++ home/.zsh.after/jenv.zsh
@@ -1,4 +1,4 @@
 eval "$(jenv init - zsh)"
-for i in maven gradle groovy scala lein springboot ; do
+for i in export maven gradle groovy scala lein springboot ; do
   [ -L $HOME/.jenv/plugins/$i ] || jenv enable-plugin $i
 done
