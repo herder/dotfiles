a00c2ccbd6f18b09b7ce0ae43d9a18d8f0c6ea51
Niklas Herder <niklas.herder@gmail.com>
2021-04-06 11:17:29 +0200

    Install pyenv(-virtualenv)   if not present

--- home/.zsh.after/zzzz_pyenv.zsh
+++ home/.zsh.after/zzzz_pyenv.zsh
@@ -1,5 +1,17 @@
+if [[ ! -a ~/.pyenv ]]; then
+  echo "Did not find pyenv installation, cloning..."
+  git clone git@github.com:pyenv/pyenv.git ~/.pyenv
+fi
+if [[ ! -a ~/.pyenv/plugins/pyenv-virtualenv ]]; then
+  echo "Did not find pyenv-virtualenv installation, cloning..."
+  git clone git@github.com:pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+fi
+
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 if which pyenv > /dev/null; then 
-  eval "$(pyenv init -)"; 
-#  export PATH="$HOME/.pyenv/shims/:${PATH}"
+  source <(pyenv init -)
+  # export PATH="$HOME/.pyenv/shims/:${PATH}"
 fi
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
