7ed7ea34ae42dcf596cd6222320bf20a683da494
Niklas Herder <niklas.herder@gmail.com>
2021-07-24 15:39:41 +0200

    Add initial Doom emacs config
    With configs for projectile, org-mode and org-super-agenda + font ligatures.

---
+++ home/.doom.d/config.el
@@ -0,0 +1,134 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Niklas Herder"
+      user-mail-address "niklas.herder@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;; (setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq
+  doom-theme 'doom-one
+  doom-font "Jetbrains Mono"
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+  display-line-numbers 'relative
+
+  +org-capture-todo-file "inbox.org"
+  org-directory "~/Dropbox/org/"
+  org-default-notes-file "~/Dropbox/org/inbox.org"
+  org-enforce-todo-dependencies 1
+  org-enforce-todo-checkbox-dependencies 1
+  org-log-done 'time
+  org-agenda-files (ignore-errors (directory-files +org-directory t "\\.org$" t))
+
+  org-roam-directory "~/Dropbox/org-roam"
+
+  projectile-project-search-path '(("~/src/git/ghe.spotify.net" . 2) "~/Projects/")
+  ;; org-super-agenda-groups '(
+  ;;                            (:name "Today"
+  ;;                              :time-grid t
+  ;;                              :scheduled today)
+  ;;                            (:name "Due today"
+  ;;                              :deadline today)
+  ;;                            (:name "Important"
+  ;;                              :priority "A")
+  ;;                            (:name "Overdue"
+  ;;                              :deadline past)
+  ;;                            (:name "Due soon"
+  ;;                              :deadline future)
+  ;;                            )
+  ;; )
+
+;; Reload all buffers when modified externally
+  )
+(global-auto-revert-mode 1)
+(auto-save-visited-mode +1)
+(use-package! org-super-agenda
+  :after org-agenda
+  :init
+  (setq org-agenda-skip-scheduled-if-done t
+      org-agenda-skip-deadline-if-done t
+      org-agenda-include-deadlines t
+      org-agenda-block-separator nil
+      org-agenda-compact-blocks t
+      org-agenda-start-day nil ;; i.e. today
+      org-agenda-span 1
+      org-agenda-start-on-weekday nil)
+  (setq org-agenda-custom-commands
+        '(("c" "Super view"
+           ((agenda "" ((org-agenda-overriding-header "")
+                        (org-super-agenda-groups
+                         '((:name "Today"
+                                  :time-grid t
+                                  :date today
+                                  :order 1)))))
+            (alltodo "" ((org-agenda-overriding-header "")
+                         (org-super-agenda-groups
+                          '((:log t)
+                            (:name "To refile"
+                                   :file-path "inbox\\.org")
+                            (:name "Next to do"
+                                   :todo "NEXT"
+                                   :order 1)
+                            (:name "Important"
+                                   :priority "A"
+                                   :order 6)
+                            (:name "Today's tasks"
+                                   :file-path "journal/")
+                            (:name "Due Today"
+                                   :deadline today
+                                   :order 2)
+                            (:name "Scheduled Soon"
+                                   :scheduled future
+                                   :order 8)
+                            (:name "Overdue"
+                                   :deadline past
+                                   :order 7)
+                            (:name "Meetings"
+                                   :and (:todo "MEET" :scheduled future)
+                                   :order 10)
+                            (:discard (:not (:todo "TODO")))))))))))
+  :config
+  (org-super-agenda-mode))
+
+(add-hook! 'focus-out-hook (lambda() (org-save-all-org-buffers)))
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
