5ce1e90e24b004ac2dd1b759ac55eaca3768ea41
Niklas Herder <niklas.herder@gmail.com>
2022-04-27 09:25:11 +0200

    Add bwssh to get SSH keys

--- home/.zsh.after/bitwarden.zsh
+++ home/.zsh.after/bitwarden.zsh
@@ -7,6 +7,14 @@ if type bw >/dev/null ; then
       bw get item "$(bw list items | jq '.[] | "\(.name) | username: \(.login.username) | id: \(.id)" ' | fzf-tmux | awk '{print $(NF -0)}' | sed 's/\"//g')" | jq '.login.password' | sed 's/\"//g' | xclip -sel clip
     fi
   }
+ bwssh() {
+   if hash bw 2>/dev/null; then
+     if ! (( ${+BW_SESSION} )); then 
+       eval $(bw unlock | grep export | cut -d ' ' -f 2-)
+     fi
+     bw get item "$(bw list items --folderid 3adf97b7-4f1f-40ad-b025-ac5b009a00df | jq '.[] | "\(.name) | notes: \(.notes) | id: \(.id)" ' | fzf-tmux | awk '{print $(NF -0)}' | sed 's/\"//g')" | jq '.fields[] | select(.name=="Passphrase") | .value' | sed 's/\"//g' | xclip -sel clip
+   fi
+ }
 
 if ! [ -f ~/.zfunc/_bw ] ; then
   echo "Creating autocomplete file for bw"
