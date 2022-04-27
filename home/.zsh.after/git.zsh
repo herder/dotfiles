ce3fa2260ad8603c6eb4111b067343b6c9776400
Niklas Herder <niklas.herder@gmail.com>
2022-04-27 21:05:25 +0200

    Remove hub alias

--- home/.zsh.after/git.zsh
+++ home/.zsh.after/git.zsh
@@ -12,6 +12,6 @@ fco() {
     fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2) || return
   git checkout $(echo "$target" | awk '{print $2}')
 }
-alias git='noglob hub'
-compdef hub=git
-compdef lab=git
+# alias git='noglob hub'
+# compdef hub=git
+# compdef lab=git
