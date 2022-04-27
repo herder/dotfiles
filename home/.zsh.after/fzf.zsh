4c5cb8d0fa2323d8201c881f0bbe18ea96cbfcfc
Niklas Herder <niklas.herder@gmail.com>
2022-04-27 21:10:07 +0200

    Don't use Homebrew fzf

--- home/.zsh.after/fzf.zsh
+++ home/.zsh.after/fzf.zsh
@@ -1,19 +1,23 @@
-# Setup fzf
-# ---------
-BREW_HOME=$(brew --prefix)
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:$BREW_HOME/opt/fzf/bin"
-fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$BREW_HOME/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "$BREW_HOME/opt/fzf/shell/key-bindings.zsh"
-
+# # Setup fzf
+if type fzf >/dev/null 2>&1; then
+  FZF_HOME="$(dirname $(which fzf))/.."
+# # ---------
+# BREW_HOME=$(brew --prefix)
+# if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+#   export PATH="$PATH:$BREW_HOME/opt/fzf/bin"
+# fi
+#
+# # Auto-completion
+# # ---------------
+[[ $- == *i* ]] && source "$FZF_HOME/shell/completion.zsh" 2> /dev/null
+#
+# # Key bindings
+# # ------------
+source "$FZF_HOME/shell/key-bindings.zsh"
+#
 export FZF_DEFAULT_COMMAND="rg --files --no-ignore --hidden --follow --glob '!{.git,Caches,node_modules}/*' 2>/dev/null"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--select-1 --exit-0"
 #export FZF_COMPLETION_TRIGGER=''
+#
+fi
