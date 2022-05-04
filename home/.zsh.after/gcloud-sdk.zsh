d8fd5cad9b6a7692349dd4cb2b251c800e677d47
Niklas Herder <niklas.herder@gmail.com>
2022-05-04 15:58:12 +0200

    Only configure gcloud if installed

--- home/.zsh.after/gcloud-sdk.zsh
+++ home/.zsh.after/gcloud-sdk.zsh
@@ -1,5 +1,7 @@
-GCLOUD_HOME="/usr/share/google-cloud-sdk"
-# source '$GCLOUD_HOME/path.zsh.inc'
-source "$GCLOUD_HOME/completion.zsh.inc"
+if type gcloud >/dev/null 2>&1; then
+  GCLOUD_HOME="/usr/share/google-cloud-sdk"
+  # source '$GCLOUD_HOME/path.zsh.inc'
+  source "$GCLOUD_HOME/completion.zsh.inc"
+fi
 
 
