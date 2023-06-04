363ea13cd206aaab8145a990d0f3b2ce70af77a2
Niklas Herder <niklas.herder@gmail.com>
2023-06-04 09:13:44 +0200

    Install GH CLI if not present

--- home/.zsh.after/gh.zsh
+++ home/.zsh.after/gh.zsh
@@ -1,2 +1,14 @@
+if ! type gh >/dev/null 2>&1 ; then
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+  && sudo apt update \
+  && sudo apt install gh -y
+
+## Add some aliases
+  gh alias set --shell co 'gh pr checkout "$(gh pr list | fzf | cut -f 1)"'gh alias set --shell "pr co" 'gh pr checkout "$(gh pr list | fzf | cut -f 1)"'
+
+fi
 source <(gh completion -s zsh)
 compdef _gh gh
