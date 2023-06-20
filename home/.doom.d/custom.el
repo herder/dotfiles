a792281d366db8a6e05c6b08346cb6cdd9ee1432
Niklas Herder <niklas.herder@gmail.com>
2023-06-20 13:12:13 +0200

    Doom doom doom

---
+++ home/.doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("016f665c0dd5f76f8404124482a0b13a573d17e92ff4eb36a66b409f4d1da410" default))
+ '(package-selected-packages '(org-gtd obsidian org-super-agenda)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
