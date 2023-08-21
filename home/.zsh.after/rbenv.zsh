9be0e4769b2f539c88d57bfeb088837cb66b2223
Niklas Herder <niklas.herder@gmail.com>
2023-08-21 11:36:02 +0200

    Install more modern rbenv
    The apt version is seriously outdated.

--- home/.zsh.after/rbenv.zsh
+++ home/.zsh.after/rbenv.zsh
@@ -1,4 +1,12 @@
-if which rbenv > /dev/null; then 
-  eval "$(rbenv init -)"; 
-  export PATH="$HOME/.rbenv/shims:${PATH}"
+if [ ! -d ~/.rbenv/ ] ; then
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 fi
+
+eval "$(~/.rbenv/bin/rbenv init - zsh)"
+
+for i in ruby-build rbenv-gem-rehash; do 
+  if [ ! -d "$(rbenv root)/plugins/$i" ] ; then 
+    echo "Installing $i for rbenv..."
+    git clone https://github.com/rbenv/$i.git "$(rbenv root)"/plugins/$i
+  fi
+done
