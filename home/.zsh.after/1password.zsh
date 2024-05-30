8ca5bd49a5bb70ef4ae883d9957193722abc8898
Niklas Herder <niklas.herder@gmail.com>
2024-05-30 08:35:52 +0200

    Fix syntax in 1password config

--- home/.zsh.after/1password.zsh
+++ home/.zsh.after/1password.zsh
@@ -1,4 +1,9 @@
 if ! type op >/dev/null 2>&1; then
-sudo $DOTFILES_PATH/install/install_1password.sh
- fi
-eval "$(op completion zsh)"; compdef _op op
+  sudo $DOTFILES_PATH/install/install_1password.sh
+  source $HOME/.config/op/plugins.sh
+  # opam configuration
+  [[ ! -r $HOME/.opam/opam-init/init.zsh ]] || source $HOME/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
+  eval "$(op completion zsh)" 
+fi
+
+
