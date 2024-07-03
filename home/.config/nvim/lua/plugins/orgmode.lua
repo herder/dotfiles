9845c1f8e68999544d3453a808ec56ba3f853b9c
Niklas Herder <niklas.herder@gmail.com>
2024-07-03 08:46:08 +0200

    Add some org capture templates

--- home/.config/nvim/lua/plugins/orgmode.lua
+++ home/.config/nvim/lua/plugins/orgmode.lua
@@ -10,9 +10,6 @@ return {
   },
   event = "VeryLazy",
   config = function()
-    -- Load treesitter grammar for org
-    require("orgmode").setup_ts_grammar()
-
     -- Setup treesitter
     require("nvim-treesitter.configs").setup({
       highlight = {
@@ -26,6 +23,40 @@ return {
     require("orgmode").setup({
       org_agenda_files = "~/Dropbox/org/**/*",
       org_default_notes_file = "~/Dropbox/org/refile.org",
+      org_capture_templates = {
+        t = {
+          description = "Task",
+          template = "* TODO %?\n %u",
+          target = "~/Dropbox/org/inbox.org",
+        },
+        T = {
+          template = "* TODO %?\n %u\n %f",
+          description = "Task with link",
+          target = "~/Dropbox/org/inbox.org",
+        },
+        j = {
+          template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
+          description = "Journal",
+          target = "~/Dropbox/org/journal/%<%Y-%m>.org",
+        },
+        description = "Event",
+        e = {
+          subtemplates = {
+            r = {
+              description = "recurring",
+              template = "** %?\n %T",
+              target = "~/Dropbox/org/calendar.org",
+              headline = "Recurring",
+            },
+            o = {
+              description = "one-time",
+              template = "** %?\n %T",
+              target = "~/Dropbox/org/calendar.org",
+              headline = "One-time",
+            },
+          },
+        },
+      },
     })
   end,
 }
