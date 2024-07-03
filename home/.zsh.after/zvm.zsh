8889f7820c277b06e3e4403a4b27c8f719f1f0d1
Niklas Herder <niklas.herder@gmail.com>
2024-07-03 08:46:52 +0200

    Try to fix global copy-paste

---
+++ home/.zsh.after/zvm.zsh
@@ -0,0 +1,64 @@
+my_zvm_vi_yank() {
+  zvm_vi_yank
+  echo -en "${CUTBUFFER}" | cbread
+}
+
+my_zvm_vi_delete() {
+  zvm_vi_delete
+  echo -en "${CUTBUFFER}" | cbread
+}
+
+my_zvm_vi_change() {
+  zvm_vi_change
+  echo -en "${CUTBUFFER}" | cbread
+}
+
+my_zvm_vi_change_eol() {
+  zvm_vi_change_eol
+  echo -en "${CUTBUFFER}" | cbread
+}
+
+my_zvm_vi_put_after() {
+  CUTBUFFER=$(cbprint)
+  zvm_vi_put_after
+  zvm_highlight clear # zvm_vi_put_after introduces weird highlighting for me
+}
+
+my_zvm_vi_put_before() {
+  CUTBUFFER=$(cbprint)
+  zvm_vi_put_before
+  zvm_highlight clear # zvm_vi_put_before introduces weird highlighting for me
+}
+
+zvm_after_lazy_keybindings() {
+  zvm_define_widget my_zvm_vi_yank
+  zvm_define_widget my_zvm_vi_delete
+  zvm_define_widget my_zvm_vi_change
+  zvm_define_widget my_zvm_vi_change_eol
+  zvm_define_widget my_zvm_vi_put_after
+  zvm_define_widget my_zvm_vi_put_before
+
+  zvm_bindkey visual 'y' my_zvm_vi_yank
+  zvm_bindkey visual 'd' my_zvm_vi_delete
+  zvm_bindkey visual 'x' my_zvm_vi_delete
+  zvm_bindkey vicmd  'C' my_zvm_vi_change_eol
+  zvm_bindkey visual 'c' my_zvm_vi_change
+  zvm_bindkey vicmd  'p' my_zvm_vi_put_after
+  zvm_bindkey vicmd  'P' my_zvm_vi_put_before
+}
+
+ if [[ $(uname) = "Darwin" ]]; then
+  on_mac_os=0
+else
+  on_mac_os=1
+fi
+
+cbread() {
+  clipcopy
+}
+
+cbprint() {
+  clippaste
+}
+
+
