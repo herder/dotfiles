d64ef3669655d3f06818e3ee8fd7d24570ba6911
Niklas Herder <niklas.herder@gmail.com>
2024-07-31 16:03:27 +0200

    Load goenv earlier
    Otherwise some completions won't load, since they check if the binary
    for them is in the PATH, and they won't be until goenv has loaded :/

--- home/.zsh.after/goenv.zsh
+++ home/.zsh.after/goenv.zsh
@@ -1,10 +1,10 @@
-if [[ ! -a ~/.goenv ]]; then
-  echo "Did not find goenv installation, cloning..."
-  git clone git@github.com:syndbg/goenv.git ~/.goenv
-fi
-
 export GOENV_ROOT="$HOME/.goenv"
 export PATH="$GOENV_ROOT/bin:$PATH"
+
+if [[ ! -a ~/.goenv ]]; then
+  echo "Did not find goenv installation, cloning..."
+  git clone git@github.com:syndbg/goenv.git $GOENV_ROOT
+fi
 
 if which goenv > /dev/null; then 
   source <(goenv init -)
@@ -12,6 +12,7 @@ if which goenv > /dev/null; then
   export PATH="$GOROOT/bin:$PATH"
   export PATH="$PATH:$GOPATH/bin"
 fi
+
 if ! type go >/dev/null 2>&1; then
   goenv install latest
   goenv versions | xargs goenv global
