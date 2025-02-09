cee4b52c2fc1247d58f25b9f833a5232549b7ce3
Niklas Herder <niklas.herder@gmail.com>
2025-02-09 15:55:32 +0100

    Support popos as well as arch

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -9,35 +9,17 @@ if [[ ! -d ~/.sdkman ]]; then
     echo "Installing $i"
     yes | sdk install $i
   done
-
-fi
-
-if ! type pacman >/dev/null 2>&1; then
-  "This is not an Arch Linux install - bailing out"
-  exit
 fi
 
 if [[ ! -d ~/.pyenv ]]; then
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-  eval $(pyenv init --)
-
+  eval "$(pyenv init --)"
 fi
+
 if ! type n >/dev/null 2>&1; then
   curl -L https://bit.ly/n-install | bash
   n latest
 fi
-
-if ! type yay >/dev/null 2>&1; then
-  echo "Install yay"
-  sudo pacman -Syu
-  sudo pacman -S --needed base-devel git
-  git clone https://aur.archlinux.org/yay.git ~/yay/
-  cd ~/yay/
-  yes | makepkg -si
-  rm -rf ~/yay
-fi
-
-type 1password >/dev/null 2>&1 && exit
 
 case "$(uname -s)" in
 Darwin)
@@ -46,7 +28,32 @@ Darwin)
   ;;
 Linux)
   # Install for linux
-  yes | yay -S 1password 1password-cli
+  type 1password >/dev/null 2>&1 && exit
+
+  if type apt >/dev/null 2>&1; then
+    echo "Installing 1password"
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo apt-get update
+    sudo apt-get install 1password -y
+  fi
+
+  if type pacman >/dev/null 2>&1; then
+    if ! type yay >/dev/null 2>&1; then
+      echo "Install yay"
+      sudo pacman -Syu
+      sudo pacman -S --needed base-devel git
+      git clone https://aur.archlinux.org/yay.git ~/yay/
+      cd ~/yay/
+      yes | makepkg -si
+      rm -rf ~/yay
+    fi
+    yes | yay -S 1password 1password-cli
+  fi
   ;;
 *)
   echo "Unsupported OS - can't install 1password"
