1da3cffe52c224a7075e9eb5b0350ad00b592294
Niklas Herder <niklas.herder@gmail.com>
2025-02-16 14:54:04 +0100

    Add atuin selfhosted sync

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -21,6 +21,17 @@ if [[ ! -d ~/.fzf ]]; then
   ~/.fzf/install
 fi
 
+if ! type gh >/dev/null; then
+  (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
+    sudo mkdir -p -m 755 /etc/apt/keyrings &&
+    out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg &&
+    cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+    sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+    sudo apt update &&
+    sudo apt install gh -y
+fi
+
 if ! type n >/dev/null 2>&1; then
   curl -L https://bit.ly/n-install | bash
   n latest
@@ -54,7 +65,7 @@ Linux)
     sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
     curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
     sudo apt-get update
-    sudo apt-get install 1password -y
+    sudo apt-get install 1password password-cli -y
   fi
 
   if type pacman >/dev/null 2>&1; then
