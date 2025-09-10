f7529beb7dec4222a6f170d0ad82a220f6a8cc1d
Niklas Herder <niklas.herder@gmail.com>
2025-09-10 07:42:32 +0200

    Add support for pr worktrees
    So you can check out a PR in a separate worktree, located in ../worktrees/<repo>/pr-<pr-number>

--- dot_zsh.after/gh.zsh
+++ dot_zsh.after/gh.zsh
@@ -1,1 +1,5 @@
-type gh >/dev/null && eval "$(gh completion -s zsh)"
+if (type gh >/dev/null ); then 
+  eval "$(gh completion -s zsh)"
+  gh worktree >/dev/null 2>&1 || gh extension install brooke-hamilton/gh-pr-worktree
+  eval "$(gh worktree completion zsh)"
+fi
