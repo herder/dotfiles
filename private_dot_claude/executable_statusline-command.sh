d2a4c179445fd02cd429fea89b2cbbbd2d1d3002
Niklas Herder <niklas@stravito.com>
2026-06-07 08:43:05 +0200

    Move ~/.claude to private repo external

--- private_dot_claude/executable_statusline-command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-input=$(cat)
-cwd=$(echo "$input" | jq -r '.workspace.current_dir // .cwd')
-
-PWD="$cwd" STARSHIP_SHELL="" starship prompt --path "$cwd" 2>/dev/null | sed 's/%{%}//g' | tr -d '\n'
