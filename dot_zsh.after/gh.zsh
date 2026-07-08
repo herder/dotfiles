7f892bd77a8dcf6868354852bbff5731585841bc
Niklas Herder <niklas.herder@gmail.com>
2026-07-08 15:33:54 +0200

    Dont use gh-worktree plugin

--- dot_zsh.after/gh.zsh
+++ dot_zsh.after/gh.zsh
@@ -1,5 +1,3 @@
 if (type gh >/dev/null ); then 
   eval "$(gh completion -s zsh)"
-  gh worktree >/dev/null 2>&1 || gh extension install brooke-hamilton/gh-pr-worktree
-  eval "$(gh worktree completion zsh)"
 fi
